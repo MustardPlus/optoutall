a329fe122ce5aabaa89c20e434962ad6ea358249
MustardPlus <mustardplusminus@gmail.com>
2022-10-03 16:31:15 -0700

    Add files via upload
    visual update #1

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-/* Romeo 12/14/21 */
+/* Romeo 10/03/22 */
+function bg_scroll() {
+	
+	let cbg = document.getElementById("cbg");
+	let trigger = document.getElementById("front_logo");
+	
+	let windowHeight = window.innerHeight;
+	
+	let elementTop = trigger.getBoundingClientRect().top;
+	
+	let mark1 = -80;
+	if (elementTop < mark1) {
+		cbg.classList.add("bg_view2");
+		cbg.classList.remove("bg_view1");
+	} else {
+		cbg.classList.add("bg_view1");
+		cbg.classList.remove("bg_view2");
+	}
+}
+
+window.addEventListener("scroll", bg_scroll);
+
